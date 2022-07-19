f854defed9812e492c9ee29ddee3d553a0846b57
Jorim Theuns <jorim.theuns@gmail.com>
2022-07-19 07:20:20 +0200

    padding

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Head><link rel="preconnect" href="https://fonts.googleapis.com" />
         <title>Strijp Spark</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet"/></Head>Y
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet"/></Head>
       <body className="p-1">
         <Main />
         <NextScript />
