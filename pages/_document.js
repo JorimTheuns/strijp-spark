7ccf12b692e41cf2837591bcb2e9946883377851
Jorim Theuns <jorim.theuns@gmail.com>
2022-07-19 07:57:07 +0200

    padding

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <title>Strijp Spark</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet"/></Head>
-      <body className="p-1">
+      <body className="">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <Head>
+    <div className="flex flex-col items-center h-screen p-1">
+      <Head> 
         <title>Strijp Spark</title>
       </Head>
 
-      <main className="flex flex-col items-center mx-auto my-auto space-y-4 max-w-lg text-center">
+      <main className="flex flex-col items-center mx-auto my-auto space-y-4 max-w-lg text-center p-1">
         <Header title="Welkom bij Strijp Spark" />
         <p className=" text-lg">
           Deze website is nog even onder constructie.
