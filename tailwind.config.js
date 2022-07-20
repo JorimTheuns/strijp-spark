f3a50100cc0cd254f225d716bee0ffe27c3c9bad
Jorim Theuns <jorim.theuns@gmail.com>
2022-07-21 00:29:41 +0200

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"],
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Space Grotesk', 'sans-serif'],
+        sans: ["Space Grotesk", "sans-serif"],
+      },
+      animation: {
+        slidel: "slide1 10s ease-in-out infinite",
+        slider: "slide2 10s ease-in-out infinite",
+      },
+      keyframes: {
+        slide1: {
+          "0%, 100%": { objectPosition: "40% 0" },
+          "50%": { objectPosition: "60% 0" },
+        },
+        slide2: {
+          "0%, 100%": { objectPosition: "60% 0" },
+          "50%": { objectPosition: "40% 0" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
