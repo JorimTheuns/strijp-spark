8bdb52e2d8dd98a710aeaefe523e882d15cd16c2
Jorim Theuns <jorim.theuns@gmail.com>
2022-07-31 09:39:39 +0200

    mobile-authors

--- components/authorCard.jsx
+++ components/authorCard.jsx
@@ -1,9 +1,11 @@
 export default function ({ authors }) {
   return (
-    <div className="flex space-x-5 items-center">
+    <div className="my-5 flex gap-2 items-center flex-wrap">
       {authors.map((author) => (
-        <div className=" box-content my-10 max-h-10 border-[#37352f] border-b-[5px] flex space-x-6 rounded-r-[2.5rem] items-center">
-          <p className=" pl-2 font-bold ">{author.fullName}</p>
+        <div className=" box-content my-5 max-h-10 min-h-min border-[#37352f] border-b-[5px] flex gap-2 rounded-r-[2.5rem] items-center">
+          <p className="leading-tight text-right pl-2 pb-1 pt-1 font-bold w-fit">
+            {author.fullName}
+          </p>
           <img
             className="overflow-visible h-20 border-[#37352f] rounded-[100%] border-[5px]"
             src={author.profilePhoto}
